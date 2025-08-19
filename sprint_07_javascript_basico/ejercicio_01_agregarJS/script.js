b1dc53513856457f6a7cf990fec414565b341565
Fernando Salvador <fsalvador@ecosistemasmexico.com>
2025-08-19 16:54:51 -0600

    ejercicios

--- sprint_07_javascript_basico/ejercicio_01_agregarJS/script.js
+++ sprint_07_javascript_basico/ejercicio_01_agregarJS/script.js
@@ -1,1 +1,9 @@
+/*Una breve descripción del propósito del script.
+El nombre del autor.
+La fecha en que se escribió o actualizó por última vez el script.*/
+
+//muestra un mensaje en el navegador
 document.write("<p>¡Hola, mundo!</p>");
+//document.write("<p>¡Es la magia de JavaScript!</p>");
+//muestra un mensaje en el navegador
+console.log("<p>¡Es la magia de JavaScript!</p>");
---
+++ sprint_07_javascript_basico/ejercicio_02_operadores/script.js
@@ -0,0 +1,6 @@
+console.log(20%3);
+
+// Math.floor(2.5) // 2
+console.log(1000 % 60);
+console.log(Math.floor((1000 % 60) / 18));
+console.log((1000 % 60) % 18);
