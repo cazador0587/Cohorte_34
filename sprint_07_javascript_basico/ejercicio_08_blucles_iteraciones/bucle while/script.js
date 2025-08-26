01909af701e72b49d293fd9424c738932d59cf56
Fernando Salvador <ferydali@gmail.com>
2025-08-26 00:30:14 -0600

    ejercicio while_for

--- sprint_07_javascript_basico/ejercicio_08_blucles_iteraciones/bucle while/script.js
+++ sprint_07_javascript_basico/ejercicio_08_blucles_iteraciones/bucle while/script.js
@@ -5,7 +5,7 @@ Escribe un bucle while que cuente atrás desde 10 y muestre "¡Feliz Año Nuevo!
 */
 
 // La cuenta comienza por 10
-let count = 10;
+/*let count = 10;
 
 // Escribe tu código aquí
 while (count >= 0) {
@@ -14,7 +14,6 @@ while (count >= 0) {
 }
 console.log("¡Feliz Año Nuevo!");
 
-//*************************************/
 
 /**
  * Eres el controlador del tren de alta velocidad "TripleTen Express", 
@@ -45,7 +44,7 @@ console.log("¡Feliz Año Nuevo!");
  */
 
 //Solición con while
-let km = 10; // Primera parada
+/*let km = 10; // Primera parada
 
 console.log('¡El "TripleTen Express" está saliendo!, Próximas paradas: ');
 
@@ -61,3 +60,39 @@ for (let i = 10; i <= 100; i+= 10) {
     console.log(`Parada a los ${i} km,`);
 }
 console.log("¡El \"TripleTen Express\" ha llegado a su destino final!");
+
+/*************************************/
+
+/**
+ * A Julia se le asignó la tarea de comprar comestibles.
+ * Después de comprar lo que necesitaba, a Julia le quedaban $15 y quería revisar todos los estantes de dulces,
+ * tomando uno de cada tipo para ella. 
+ * 
+ * Cada golosina cuesta $2. No tiene el suficiente dinero para comprar todos los dulces de la tienda. 
+ * Utiliza un bucle while para mostrar cuántos dulces compró. 
+ * Cada vez que Julia compre una golosina, muestra Julia compró n dulces en la consola,
+ * donde n es el total acumulado de dulces que compró a partir de la iteración actual. 
+ * Después de salir del bucle, muestra en la consola la cantidad de dinero (si queda) que le queda a Julia.
+ * 
+ **/
+
+const candyPrice = 2;
+let candiesBought = 0;
+let moneyLeft = 15;
+
+/*while (moneyLeft >= candyPrice) {
+    candiesBought++;
+    moneyLeft -= candyPrice;
+    console.log(`Julia compró ${candiesBought} dulces`);
+}
+console.log(`A Julia le quedan $ ${moneyLeft}`);
+/**
+ * 
+ */
+while (moneyLeft >= candyPrice) {
+  candiesBought += 1;
+  console.log(`Julia compró ${candiesBought} dulces`);
+
+  moneyLeft = moneyLeft - candyPrice;
+}
+console.log(`A Julia le quedan $ ${moneyLeft}`);
