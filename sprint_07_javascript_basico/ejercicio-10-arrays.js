d485499e3b537c4b2f1fd4fe6e2dc4b2ea8535e2
Fernando Salvador <fsalvador@ecosistemasmexico.com>
2025-08-27 17:23:28 -0600

    ejercicio-10

---
+++ sprint_07_javascript_basico/ejercicio-10-arrays.js
@@ -0,0 +1,44 @@
+/**
+ * Crea una variable llamada morningList (Lista de la mañana) y conviértelo en un array de actividades 
+ * matutinas añadiendo los siguientes strings:
+
+"Levantarse de la cama"
+
+"Cepillarse los dientes"
+
+"Ver las redes sociales"
+
+"Desayunar"
+
+Muestra el array resultante en la consola.
+ * 
+ 
+let morningList = [
+    "Levantarse de la cama",
+    "Cepillarse los dientes",
+    "Ver las redes sociales",
+    "Desayunar"
+];
+
+console.log(morningList);*/
+
+/**
+ * Muestra en la consola el primer y el último elemento del array. 
+ * Encuentra el índice del último elemento utilizando la propiedad length.
+ * Recuerda que el primer elemento de un array tiene el índice 0.
+ * 
+ */
+
+let morningList = [
+  'Levantarse de la cama',
+  'Cepillarse los dientes',
+  'Ver las redes sociales',
+  'Desayunar'
+];
+
+// Primer elemento → índice 0
+console.log("Primer elemento:", morningList[0]);
+
+// Último elemento → índice = length - 1
+let ultimoIndice = morningList.length - 1;
+console.log("Último elemento:", morningList[ultimoIndice]);
